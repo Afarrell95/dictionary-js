99b1bd5e18b606e6407316ffd553465fd0971769
Allison <farrella95@gmail.com>
2023-12-22 18:20:15 -0500

    color change

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
+const audioError = document.querySelector(".audio-error");
 
 btn.addEventListener("click", () => {
   let inpWord = document.getElementById("inp-word").value;
